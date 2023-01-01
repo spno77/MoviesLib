d8bdc54b826928368ee9b669788ba14f096e1a97
spno77 <pilonoti@yahoo.com>
2023-01-01 21:29:46 +0200

    graphic lib example

---
+++ project/config.h
@@ -0,0 +1,11 @@
+#pragma once 
+
+//Window 
+#define WINDOW_WIDTH   1200
+#define WINDOW_HEIGHT   600
+//Canvas
+#define CANVAS_WIDTH  140.0f 
+#define CANVAS_HEIGHT 80.0f
+
+#define SETCOLOR(c,r,g,b) { c[0] =r; c[1] = g; c[2] = b;}
+
--- project/main.cpp
+++ project/main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include <sgg/graphics.h>
+#include <string>
+
+#include "config.h"
+
+using namespace std;
 
 
-int main(int argc, char const *argv[])
+void update(float ms)
 {
-	
-	printf("Hello, world\n");
+   
+}
+ 
 
-	return 0;
+void draw()
+{
+    
+}
+ 
+int main()
+{
+    graphics::createWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"MoviesLib");
+ 
+    graphics::setDrawFunction(draw);
+    graphics::setUpdateFunction(update);
+ 
+    graphics::setCanvasSize(CANVAS_WIDTH,CANVAS_HEIGHT);
+    graphics::setCanvasScaleMode(graphics::CANVAS_SCALE_FIT);
+    
+    graphics::Brush br;
+ 	SETCOLOR(br.fill_color,0.4,0.3,0.8);
+ 	graphics::setWindowBackground(br);
+    
+    graphics::startMessageLoop();
+ 
+    return 0;
 }
