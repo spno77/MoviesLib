2c9391d28b91e4a12e890d01d33971ccbdc45e56
spno77 <pilonoti@yahoo.com>
2023-01-01 20:56:28 +0200

    Skeleton files

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CC=g++
+LD=g++
+BUILD_PATH="build/Release"
+BUILD_PATH_DEBUG="build/Debug"
+BIN_PATH="bin"
+LIB_PATH="lib"
+CFLAGS="-O2 -std=c++17"
+CFLAGS_DEBUG="-Og -g -std=c++17"
+
+mkdir -p build
+mkdir -p $BIN_PATH
+mkdir -p $BIN_PATH/assets
+mkdir -p $BUILD_PATH
+mkdir -p $BUILD_PATH_DEBUG
+
+$CC $CFLAGS -L$LIB_PATH -I. project/main.cpp  -o $BIN_PATH/main -lsgg -lGL -lGLEW -lSDL2 -lSDL2_mixer -lfreetype -lstdc++fs
+
+$CC $CFLAGS_DEBUG -L$LIB_PATH -I. project/main.cpp  -o $BIN_PATH/maind -lsggd -lGL -lGLEW -lSDL2 -lSDL2_mixer -lfreetype -lstdc++fs
+
---
+++ project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sgg/graphics.h>
+
+
+int main(int argc, char const *argv[])
+{
+	
+	printf("Hello, world\n");
+
+	return 0;
+}
